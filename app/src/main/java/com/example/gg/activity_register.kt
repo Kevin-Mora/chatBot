5534a8f2d7ab8fbc169deb248190de9555556a02
silviasalazar <silvia.salazar18@tectijuana.edu.mx>
2021-05-08 13:58:32 -0700

    modificacion botones welcome activity

--- app/src/main/java/com/example/gg/activity_register.kt
+++ app/src/main/java/com/example/gg/activity_register.kt
@@ -2,6 +2,13 @@ package com.example.gg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+import kotlinx.android.synthetic.main.activity_register.*
 
 class activity_register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
