231432414211848480c00fa5521db252972c99b2
silviasalazar <silvia.salazar18@tectijuana.edu.mx>
2021-05-08 21:38:56 -0700

    implementacion BD register

--- app/src/main/java/com/example/gg/activity_login.kt
+++ app/src/main/java/com/example/gg/activity_login.kt
@@ -5,11 +5,26 @@ import android.os.Bundle
 import android.widget.Button;
 import android.widget.TextView
 import com.example.gg.ui.MainActivity
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class activity_login : AppCompatActivity() {
+    private lateinit var txtEmail: EditText
+    private lateinit var txtPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        txtEmail=findViewById(R.id.txtEmail)
+        txtPassword=findViewById(R.id.txtPassword)
     }
 }
