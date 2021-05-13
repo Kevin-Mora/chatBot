dfbd4edede22187ccd147534477c96fe64d3a7e3
silviasalazar <silvia.salazar18@tectijuana.edu.mx>
2021-05-12 23:03:49 -0700

    Actionbar

--- app/src/main/java/com/example/gg/ActivityWelcome.kt
+++ app/src/main/java/com/example/gg/ActivityWelcome.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class activity_welcome : AppCompatActivity() {
+class ActivityWelcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
